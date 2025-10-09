ccdc7d64a4b5419782821ef47749af5ba4fc0286
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-10-09 20:18:53 +0600

    project setup done

---
+++ src/utils/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Button({ children }) {
+    return (
+        <button
+            className="relative overflow-hidden px-6 py-3 font-semibold text-white 
+        bg-gradient-to-r from-[#A26E26] to-[#E5BA5E] 
+        transition-all duration-500 ease-in-out 
+        hover:from-[#E5BA5E] hover:to-[#A26E26] 
+        shadow-md hover:shadow-lg">
+            <span className="relative z-10">{children}</span>
+        </button>
+    )
+}
+
+export default Button
